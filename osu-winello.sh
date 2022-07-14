1bcbd8e55d1c2e308aa17b84aa69de7c4dd15b46
Felipe Paixão <fleap@fleap.dev>
2022-07-14 15:39:30 -0300

    Fix multilib being added every time

--- osu-winello.sh
+++ osu-winello.sh
@@ -91,8 +91,10 @@ function install()
     Info "Please enter your password when asked"
     Info "------------------------------------"
 
-    Info "Enabling multilib.."
-    ! grep "osu-install" >/dev/null 2>&1 < /etc/pacman.conf && printf "\n# Multilib repo enabled by osu-winello\n[multilib]\nInclude = /etc/pacman.d/mirrorlist\n" | sudo tee -a /etc/pacman.conf
+    if ! grep -q -E '^\[multilib\]' '/etc/pacman.conf'; then
+        Info "Enabling multilib.."
+        printf "\n# Multilib repo enabled by osu-winello\n[multilib]\nInclude = /etc/pacman.d/mirrorlist\n" | sudo tee -a /etc/pacman.conf
+    fi
 
     Info "Installing packages and wine-staging dependencies.."
     sudo pacman -Sy --noconfirm --needed git base-devel p7zip wget zenity wine-staging winetricks giflib lib32-giflib libpng lib32-libpng libldap lib32-libldap gnutls lib32-gnutls mpg123 lib32-mpg123 openal lib32-openal v4l-utils lib32-v4l-utils libpulse lib32-libpulse alsa-plugins lib32-alsa-plugins alsa-lib lib32-alsa-lib libjpeg-turbo lib32-libjpeg-turbo libxcomposite lib32-libxcomposite libxinerama lib32-libxinerama ncurses lib32-ncurses opencl-icd-loader lib32-opencl-icd-loader libxslt lib32-libxslt libva lib32-libva gtk3 lib32-gtk3 gst-plugins-base-libs lib32-gst-plugins-base-libs vulkan-icd-loader lib32-vulkan-icd-loader cups samba dosbox || Error "Some libraries didn't install for some reason, check pacman or your connection"
