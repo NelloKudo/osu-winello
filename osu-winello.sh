d431a50fe7b8d7816a186a5f4be9ddbebc3bb70e
NelloKudo <98063377+NelloKudo@users.noreply.github.com>
2024-08-10 04:00:48 +0200

    bump wine-osu-wow64-9.14-staging!
    New release with a major feature: WoW64!
    This means lib32 libraries *shouldn't* be required anymore for the game to run properly, but I'll change the script accordingly only later on. Read more at here: https://github.com/NelloKudo/WineBuilder/releases/tag/wine-osu-wow64-staging-9.14

--- osu-winello.sh
+++ osu-winello.sh
@@ -8,7 +8,7 @@
 #   =======================================
 
 # Wine-osu current versions for update
-WINEVERSION=9.14.0
+WINEVERSION=9.14.2
 LASTWINEVERSION=0 
 
 # Current version of GLIBC on the user's system (needed to check if wine-osu builds are compatible or not)
@@ -16,8 +16,8 @@ CURRENTGLIBC="$(ldd --version | tac | tail -n1 | awk '{print $(NF)}')"
 MINGLIBC=2.31
 
 # Wine-osu mirrors
-WINELINK="https://github.com/NelloKudo/WineBuilder/releases/download/wine-osu-staging-9.14/wine-osu-9.14-staging-x86_64.tar.xz"
-WINEBACKUPLINK="https://www.dropbox.com/scl/fi/oskcxlb9obvpzggg3y11c/wine-osu-9.14-staging-x86_64.tar.xz?rlkey=tv1fiktsnsvdzhzy6g4o1ov8n&st=f2jhwea2&dl=0"
+WINELINK="https://github.com/NelloKudo/WineBuilder/releases/download/wine-osu-wow64-staging-9.14/wine-osu-wow64-9.14-staging-x86_64.tar.xz"
+WINEBACKUPLINK="https://www.dropbox.com/scl/fi/ufaxrabp2nnwfneohwpr2/wine-osu-wow64-9.14-staging-x86_64.tar.xz?rlkey=3k0xw8eq563sn8k88flfnccan&st=hdhp4pcy&dl=0"
 
 # Checking for --no-deps flag:
 USEDEPS="true"
